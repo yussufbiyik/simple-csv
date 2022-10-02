d201c59d23a585f5a225abd2a7f3189668765274
Yusuf Bıyık <yusufbyk3660@gmail.com>
2022-10-03 00:22:22 +0300

    ⏫ Code the stringify method - Code stringify - Add surroundWithDoubleQuote fucntion - Add findObjectWithLongestKeys function (not coded yet!)

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+// I have not tested this so don't judge.
+
+const quoteRegex = /"/gmi;
+
+function surroundWithDoubleQuote(string, isSurroundedWithDoubleQuote) {
+    // Surround already existing double quotes with another set of double quotes..
+    // ..so it doesn't cause any problems. 
+    string.replace(quoteRegex, '""'); 
+    if(isSurroundedWithDoubleQuote) return `"${string}"`;
+    return string;
+}
+
+function findObjectWithLongestKeys(objects) {
+    // some code
+}
+
 exports.parse = function (csvFile) {
     // Parse a csv file then return an object
 }
 
-exports.stringify = function (object) {
+exports.stringify = function (objects, options) {
     // Turn an object to a CSV file
+    const seperator = options.seperator ? options.seperator : ",";
+    const isSurroundedWithDoubleQuote = options.isSurroundedWithDoubleQuote ? options.isSurroundedWithDoubleQuote : false;
+    // Headers are generated based on the object with the longest length of keys.
+    const headers = Object.keys(findObjectWithLongestKeys(objects)).map(key => surroundWithDoubleQuote(key, isSurroundedWithDoubleQuote)).join(seperator);
+    let csv = headers;
+    // Get values of each object and add them to the csv string
+    for (let index = 0; index < objects.length; index++) {
+        const object = objects[index];
+        const data = Object.values(object).map(value => surroundWithDoubleQuote(value, isSurroundedWithDoubleQuote)).join(seperator);
+        csv += `\n${data}`;
+        if(index === objects.length - 1) {
+            return csv;
+        }
+    }
 }
