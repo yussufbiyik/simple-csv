57bf7f93b993333cbb18178cb8c146873653b976
Yusuf Bıyık <yusufbyk3660@gmail.com>
2022-10-04 21:30:22 +0300

    ⏫ Add .parse & Update inline documentation of .stringify & .parse
    - Add .parse method & inline documentation
    - Update inline documentation
    - Add more testing data for parsing
    - Update testing code
    * Combine stringify test commands into one single function
    * Add a function to test .parse method

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const quoteRegex = /"/gm;
+const newLineRegex = /(\r\n|\r|\n)/gm;
+const twoDoubleQuoteRegex = /""/gm;
 
 function surroundWithDoubleQuote(string, isSurrounded) {
     if(isSurrounded === true) {
@@ -17,19 +19,53 @@ function findObjectWithLongestKeys(objects) {
     return objects[objectWithLongestKeys.index]
 }
 
-// Return an object by parsing a CSV string
+function unSurround(string) {
+    return string.trim().substring(1, string.trim().length-1).replace(twoDoubleQuoteRegex, '"')
+}
+
+/**
+ * Returns an array of objects by parsing a CSV string.
+ * @param {string} csv Source of the object as a string containing CSV data 
+ * @param {object=} [options={seperator:",", isSurrounded: true}] Specifies the properties of the output
+ * @param {string} options.seperator Seperator that is used in the CSV to seperate the values
+ * @param {Boolean} options.isSurrounded Specifies if the values are seperated by double quotes, if true, assumes that all the values are surrounded by double quotes
+ * @returns {object[]} Object containing parsed CSV data from the passed CSV string
+ */
 exports.parse = function (csv, options) {
-    // some code
+    let seperator = ",";
+    let isSurrounded = false;
+    if(options) {
+        seperator = options.seperator ? options.seperator : seperator;
+        isSurrounded = (options.isSurrounded == true) ? options.isSurrounded : isSurrounded;
+    }
+    const lines = csv.split(newLineRegex).filter(line => line.length>1).map(line => line.trim());
+    const firstLine = lines.shift();
+    lines.pop();
+    const keys = firstLine.split(seperator)
+        .map(key => (isSurrounded == false) ? key.trim() : unSurround(key))
+    const parsedCsvArray = []
+    for (let index = 0; index < lines.length; index++) {
+        const line = lines[index];
+        const entries = line.split(seperator).map(entry => (isSurrounded == false) ? entry.trim() : unSurround(entry));
+        const parsedCsvObject = {};
+        keys.forEach((key, index) => {
+            if(index <= entries.length-1) {
+                parsedCsvObject[key] = entries[index]
+            }
+        })
+        parsedCsvArray.push(parsedCsvObject)
+    }
+    return parsedCsvArray
 }
 
 /**
  * Returns a CSV string with the given objects and options.
  * Amount of keys in an object does not matter, only things that matter are order of the keys and their names.
- * @param {array} objects Source of the CSV string as an array of objects  
+ * @param {object[]} objects Source of the CSV string as an array of objects  
  * @param {object=} [options={seperator:",", isSurrounded: true}] Specifies the properties of the output
  * @param {string} options.seperator Seperator that is going to be used to seperate the values
  * @param {Boolean} options.isSurrounded Specifies if the value is going to be surrounded with double quotes
- * @returns {string} string with CSV data
+ * @returns {string} A string containing stringified object data from the passed array of objects. 
  */
 exports.stringify = function (objects, options) {
     let seperator = ","
--- test.js
+++ test.js
@@ -32,7 +32,17 @@ const objects = [
     },
 ]
 
-// Dummy CSV files can be downloaded from https://people.sc.fsu.edu/~jburkardt/data/csv/csv.html maybe?
+// Dummy CSV strings from https://people.sc.fsu.edu/~jburkardt/data/csv/csv.html
+const csvStrings = [
+    `"Day of Week", "Number of Crashes"
+    "Sunday is the best ""day""", "13664"
+    "Monday is the worst ""day""", "17279"
+    "Tuesday", "17337"
+    "Wednesday", "17394"
+    "Thursday", "17954"
+    "Friday", "19147"
+    "Saturday", "15714"`
+]
 
 // Dummy options
 const optionsArray = [
@@ -61,12 +71,20 @@ const optionsArray = [
     }
 ]
 
-console.log(
-    `\n[[ no options ]]]\n${CSV.stringify(objects)}\n[[[ no options ]]]`
-)
-optionsArray.forEach(options => {
-    console.log(options)
-    console.log(
-        `\n[[[ ${options.identifier} ]]]\n${CSV.stringify(objects, options.options)}\n[[[ ${options.identifier} ]]]`
-    )
-})
+function testStringify() {
+    console.log(`\n[[ no options ]]]\n${CSV.stringify(objects)}\n[[[ no options ]]]`)
+    optionsArray.forEach(options => {
+        console.log(options)
+        console.log(
+            `\n[[[ ${options.identifier} ]]]\n${CSV.stringify(objects, options.options)}\n[[[ ${options.identifier} ]]]`
+        )
+    })
+}
+
+function testParse() {
+    console.log(CSV.parse(csvStrings[0], {seperator:',', isSurrounded:true}))
+    // console.log(CSV.parse(csvStrings[1], {seperator:',', isSurrounded:false}))
+}
+
+testParse()
+testStringify()
